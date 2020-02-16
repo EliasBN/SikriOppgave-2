99fd55f9eeb541f55b02135b099de1e69feb8e2c
Elias <eliasn16@uia.no>
2020-02-16 13:33:07 +0100

    Add project files.

---
+++ SikriOppgave/ListeService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace SikriOppgave
+{
+    
+
+// The main class for performing operations on a List. 
+    class ListeService
+    {
+
+        // Checks if a value is an EvenNumber. 
+        public ListeService isEvenNumber(int value)
+        {
+            if (value % 2 == 0)
+            {
+                Console.WriteLine("{0} is an even number!", value);
+            }
+            return this;
+        }
+
+        // Prints out a line if a number(int value) is divisible by 7. 
+        public ListeService printSeven(int value) 
+        {
+
+            if (value % 7 == 0 && value != 0)
+            {
+                Console.WriteLine("{0} is divisble by 7.", value);
+            }
+            return this;
+        }
+
+        /// Simple implementation of the popular math game FizzBuzz. Prints out 
+        /// Fizz if a value is divisible by 5, Buzz if divisible by 3, and fizzbuzz if 
+        /// divisible by both. 
+        public ListeService fizzBuzz(int value)
+        {
+
+            if (value % 15 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (value % 5 == 0)
+            {
+                Console.WriteLine("Fizz!");
+            }
+            else if (value % 3 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            return this;
+        }
+    }
+}
---
+++ SikriOppgave/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace SikriOppgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListeService listeService = new ListeService();
+            ArrayList a = populateArray(100);
+
+            // Kunne muligens ha vært en egen metode. 
+            foreach (int e in a)
+            {
+                listeService
+                    .printSeven(e)
+                    .fizzBuzz(e)
+                    .isEvenNumber(e);
+            }
+        }
+
+
+        private static ArrayList populateArray(int length)
+        {
+            ArrayList a = new ArrayList();
+
+            for (int counter = 0; counter <= length; counter++)
+            {
+                a.Add(counter);
+            }
+            return a;
+        }
+    }
+}
+
+
+
